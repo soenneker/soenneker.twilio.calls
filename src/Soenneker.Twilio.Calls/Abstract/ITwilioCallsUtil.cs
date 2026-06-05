a25ada4f160f0accf74777584c190b29f473471f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:55 -0500

    xml commenting

--- src/Soenneker.Twilio.Calls/Abstract/ITwilioCallsUtil.cs
+++ src/Soenneker.Twilio.Calls/Abstract/ITwilioCallsUtil.cs
@@ -11,7 +11,23 @@ namespace Soenneker.Twilio.Calls.Abstract;
 /// </summary>
 public interface ITwilioCallsUtil
 {
+    /// <summary>
+    /// Gets all calls for number.
+    /// </summary>
+    /// <param name="phoneNumber">The phone number.</param>
+    /// <param name="startTimeAfter">The start time after.</param>
+    /// <param name="startTimeBefore">The start time before.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<List<CallResource>> GetAllCallsForNumber(string phoneNumber, DateTimeOffset? startTimeAfter = null, DateTimeOffset? startTimeBefore = null, CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Gets all calls for numbers split by number.
+    /// </summary>
+    /// <param name="phoneNumbers">The phone numbers.</param>
+    /// <param name="startTimeAfter">The start time after.</param>
+    /// <param name="startTimeBefore">The start time before.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<Dictionary<string, List<CallResource>>> GetAllCallsForNumbersSplitByNumber(IEnumerable<string> phoneNumbers, DateTimeOffset? startTimeAfter = null, DateTimeOffset? startTimeBefore = null, CancellationToken cancellationToken = default);
 }
