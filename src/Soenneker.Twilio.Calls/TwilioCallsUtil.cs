69144db7af3ed0e1fc505c63ea238f1a95bcf3fe
soenneker <jake@soenneker.com>
2026-09-03 20:28:15 -0500

    Remove transitive async extension dependency

--- src/Soenneker.Twilio.Calls/TwilioCallsUtil.cs
+++ src/Soenneker.Twilio.Calls/TwilioCallsUtil.cs
@@ -6,8 +6,6 @@ using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
 using Twilio.Base;
 using Soenneker.Twilio.Client.Abstract;
-using Soenneker.Extensions.ValueTask;
-using Soenneker.Extensions.Task;
 using System.Threading;
 
 namespace Soenneker.Twilio.Calls;
@@ -24,13 +22,13 @@ public sealed class TwilioCallsUtil : ITwilioCallsUtil
     public async ValueTask<List<CallResource>> GetAllCallsForNumber(string phoneNumber, DateTimeOffset? startTimeAfter = null, DateTimeOffset? startTimeBefore = null, 
         CancellationToken cancellationToken = default)
     {
-        await _twilioClientUtil.Init(cancellationToken).NoSync();
+        await _twilioClientUtil.Init(cancellationToken).ConfigureAwait(false);
         cancellationToken.ThrowIfCancellationRequested();
 
         ResourceSet<CallResource>? result = await CallResource.ReadAsync(
             to: new PhoneNumber(phoneNumber),
             startTimeAfter: startTimeAfter?.UtcDateTime,
-            startTimeBefore: startTimeBefore?.UtcDateTime).NoSync();
+            startTimeBefore: startTimeBefore?.UtcDateTime).ConfigureAwait(false);
 
         if (result is null)
             return [];
@@ -48,7 +46,7 @@ public sealed class TwilioCallsUtil : ITwilioCallsUtil
     public async ValueTask<Dictionary<string, List<CallResource>>> GetAllCallsForNumbersSplitByNumber(IEnumerable<string> phoneNumbers, DateTimeOffset? startTimeAfter = null, 
         DateTimeOffset? startTimeBefore = null, CancellationToken cancellationToken = default)
     {
-        await _twilioClientUtil.Init(cancellationToken).NoSync();
+        await _twilioClientUtil.Init(cancellationToken).ConfigureAwait(false);
 
         var callsByNumber = new Dictionary<string, List<CallResource>>();
 
@@ -59,7 +57,7 @@ public sealed class TwilioCallsUtil : ITwilioCallsUtil
             ResourceSet<CallResource>? calls = await CallResource.ReadAsync(
                 to: new PhoneNumber(phoneNumber),
                 startTimeAfter: startTimeAfter?.UtcDateTime,
-                startTimeBefore: startTimeBefore?.UtcDateTime).NoSync();
+                startTimeBefore: startTimeBefore?.UtcDateTime).ConfigureAwait(false);
 
             if (calls is null)
             {
